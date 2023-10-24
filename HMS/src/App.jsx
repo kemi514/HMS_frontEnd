35d7f553369ef5507c07cb2ac5bc8263355a12b1
kemi514 <priscillakemigisha030@gmail.com>
2023-10-24 09:37:12 +0300

    Added files to HMS_frontend repositort

---
+++ HMS/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Login from './pages/Login'
+import Home from './pages/Home'
+
+const App = () => {
+  return (
+    <>
+
+    {/* <Login/> */}
+    <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/components/Comp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Comp = () => {
+  return (
+    <p> Hello word!!!!!!!!!!!!!</p>
+  )
+}
+
+export default Comp
