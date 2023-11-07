4685f710439cfe1720db41909f671d8bb849941e
kemi514 <priscillakemigisha030@gmail.com>
2023-11-07 14:04:22 +0300

    modified app.jsx

--- react/HMS/src/App.jsx
+++ react/HMS/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Login from './pages/Login'
 import Home from './pages/Home'
 import Hostels from './pages/Hostels'
+import Profile from './pages/Profile'
+import Reservation from './pages/Reservation'
 import { DataProvider } from './utilities/DataContext'
 import { AuthProvider } from './utilities/AuthContext'
 import { Routes, Route } from 'react-router-dom'
@@ -17,6 +19,8 @@ const App = () => {
           <Route path='/signup' element={<Signup/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/hostels' element={<Hostels/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/reservation' element={<Reservation/>}/>
         </Routes>
       </AuthProvider>
     </DataProvider>
