41fd9d4685e68f825c23cdb92e0ba674d29ffacb
kemi514 <priscillakemigisha030@gmail.com>
2023-11-07 14:01:46 +0300

    added my profile component

---
+++ react/HMS/src/components/MyProfile.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const MyProfile = () => {
+  return (
+    <>
+    <div className='all-items'>
+      <div className="p-ribbon">
+        <div className="p-image">
+        <img
+        src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        className="circle comment-i"
+        alt=""
+      />
+        </div>
+     </div>
+   
+     <div className='my-prof'>
+        <smalL className="prof1">Kemigisha Priscilla</smalL> <br /> 
+        <smalL className="prof2">kemigisha.priscilla@students.mak.ac.ug</smalL> <br />
+        <smalL className="prof3">PO BOX, 345, Rwanda</smalL> <br /> 
+        <smalL className="prof3">076766565653</smalL>
+     </div>
+     </div>
+    </>
+  )
+}
+
+export default MyProfile
